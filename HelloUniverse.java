8ca2f95c46625a249afad4902274c130ad506c3a
Usman Sajid <67691352+code190715@users.noreply.github.com>
2020-12-28 21:00:57 +0000

    Add files via upload

---
+++ HelloUniverse.java
@@ -0,0 +1,10 @@
+class HelloUniverse {
+
+	public static void main (String[] args)
+	{
+		System.out.println("Hello Universe. ");
+		System.out.println("2021 is the start... God willing.");
+
+	}
+
+}
